fb091ed93dc39166284c2dc7f8e49a7f83f2d194
bowenx2002 <132802069+bowenx2002@users.noreply.github.com>
2025-12-17 15:42:40 +0800

    Add files via upload

---
+++ MyExtractor.java
@@ -0,0 +1,41 @@
+package extractbot;
+
+
+import extractbot.tool.BaseExtractor;
+
+public class MyExtractor extends BaseExtractor{
+
+	@Override
+	public int[][] getControlFlowGraphInArray(String pathFile, String methodName) 
+	{
+		/** TODO
+		 * 	Jifeng: Write down your source code here. 
+		 */
+		
+		return null;
+	}
+
+
+
+	@Override
+	public int[][] getTestRequirementsInArray(String pathFile, String methodName) 
+	{
+		/** TODO
+		 * 	Jifeng: Write down your source code here. 
+		 */
+		
+		return null;
+	}
+
+	
+	@Override
+	public int[][] getTestPathsInArray(String pathFile, String methodName) 
+	{
+		/** TODO
+		 * 	Jifeng: Write down your source code here. 
+		 */
+		
+		return null;
+	}
+	
+}
